227c003ecc029ed5c955acf077ff56e3a284e11f
Genrich Sarbaj <gsarbaj@Genrichs-MacBook-Pro.local>
2021-11-29 12:28:15 +0200

    strapi gatsby installed

---
+++ back/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'df8219434bf2c41db8d80c7195e306c5'),
+    },
+  },
+});
---
+++ back/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '7dd5570c-0ef3-4d3b-8660-9df1941617a0'
+};
